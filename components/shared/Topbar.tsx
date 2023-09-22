43efa2744600bb55225d542f641c404f043c65b3
abuhasanmahmud <mahmud.me420@gmail.com>
2023-09-22 19:05:13 +0600

    FeatV(-1) topbar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Topbar = () => {
-  console.log("topbar");
+  // console.log("topbar");
   return (
     <>
       <nav className="topbar">
