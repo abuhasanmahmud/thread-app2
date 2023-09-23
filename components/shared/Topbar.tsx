7d5863e3295a8c18e814c24ecdc406323c84e894
abuhasanmahmud <mahmud.me420@gmail.com>
2023-09-23 21:36:06 +0600

    feat(V-1) leftsidebar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 
 const Topbar = () => {
   console.log("topbar");
@@ -7,9 +8,30 @@ const Topbar = () => {
     <>
       <nav className="topbar">
         <Link href="/" className="flex items-center gap-4">
-          <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+          <Image src="/logo.svg" alt="logo" width={28} height={28} />
           <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
         </Link>
+
+        <div className="flex items-center gap-1">
+          <div className="block md:hidden">
+            <SignedIn>
+              <SignOutButton>
+                <div className="flex cursor-pointer">
+                  <Image src="/assets/logout.svg" alt="logout" width={24} height={24} />
+                </div>
+              </SignOutButton>
+            </SignedIn>
+          </div>
+
+          <OrganizationSwitcher
+            appearance={{
+              // baseTheme: dark,
+              elements: {
+                organizationSwitcherTrigger: "py-2 px-4",
+              },
+            }}
+          />
+        </div>
       </nav>
     </>
   );
