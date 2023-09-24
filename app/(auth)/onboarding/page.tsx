5fbc52f2021ee5bd4f125b71fbc81750741ed796
abuhasanmahmud <mahmud.me420@gmail.com>
2023-09-24 20:07:04 +0600

    feat(V-1) acoun profile page

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,3 +1,4 @@
+import AccountProfile from "@/components/forms/AccountProfile";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
@@ -6,6 +7,7 @@ import { redirect } from "next/navigation";
 
 async function Page() {
   const user = await currentUser();
+  // console.log("user current===", user?.username);
   if (!user) return null; // to avoid typescript warnings
 
   // const userInfo = await fetchUser(user.id);
@@ -14,11 +16,21 @@ async function Page() {
   // const userData = {
   //   id: user.id,
   //   objectId: userInfo?._id,
-  //   username: userInfo ? userInfo?.username : user.username,
+  //   username: userInfo ? userInfo?.username : user?.username,
   //   name: userInfo ? userInfo?.name : user.firstName ?? "",
   //   bio: userInfo ? userInfo?.bio : "",
   //   image: userInfo ? userInfo?.image : user.imageUrl,
   // };
+
+  const userInfo = {};
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName || "",
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl,
+  };
 
   return (
     <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
@@ -26,7 +38,7 @@ async function Page() {
       <p className="mt-3 text-base-regular text-light-2">Complete your profile now, to use Threds.</p>
 
       <section className="mt-9 bg-dark-2 p-10">
-        {/* <AccountProfile user={userData} btnTitle='Continue' /> */}
+        <AccountProfile user={userData} btnTitle="Continue" />
       </section>
     </main>
   );
